b5468c537d0a86e9f30d0e982d5d807976ad7631
sqh <838574833@qq.com>
2022-05-28 19:50:56 +0800

    第一次提交

---
+++ hellogit/src/com/sqh/HelloGit.java
@@ -0,0 +1,11 @@
+package com.sqh;
+
+/**
+ * @Author sqh
+ * @Date 2022-05-28 18:29
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
